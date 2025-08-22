a1edc2ce5e295090fbd0b613b87f1228a855aebc
Carlos Canet <carlos@carlosca.net>
2025-08-22 08:58:06 +0200

    init: starting

---
+++ README.md
@@ -0,0 +1,5 @@
+# Carlos Canet's Developer Portfolio
+
+This repository is dedicated to showcase Carlos development's skills.
+
+It's a work in progress, be patient my friend...
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title">Carlos Canet</h1>
+      <h2 className="subtitle">Developer</h2>
+      <p>The projects will be here soon...</p>
+    </>
+  )
+}
+
+export default App
