f6a2fb262e52904144f900363e49b211b84f0683
Carlos Canet <carlos@carlosca.net>
2026-02-18 19:16:05 +0100

    refactor: prettify confettiTitle and Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import TechnologiesCard from "@/features/Technologies/components/TechnologiesCar
 export default function Home() {
   return (
     <div className="flex min-h-screen items-start justify-center mt-10 bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full  flex-col items-center justify-start px-8 py-16 bg-white dark:bg-black gap-2" >
+      <main className="flex w-full flex-col items-center justify-start px-8 py-16 bg-white dark:bg-black gap-2">
         <ConfettiTitle />
         <TechnologiesCard />
         <ProjectListCard />
--- src/features/About/components/ConfettiTitle.tsx
+++ src/features/About/components/ConfettiTitle.tsx
@@ -5,7 +5,7 @@ import { Confetti, ConfettiRef } from "../../../components/ui/confetti";
 import { Shape } from "canvas-confetti";
 
 function ConfettiTitle() {
-  const confettiRef = useRef<ConfettiRef>(null)
+  const confettiRef = useRef<ConfettiRef>(null);
   return (
     <div className="relative flex h-125 w-full flex-col items-center justify-center overflow-hidden">
       <span className="pointer-events-none bg-linear-to-b from-black to-orange-600/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-orange-500 dark:to-orange-500/30">
@@ -18,11 +18,18 @@ function ConfettiTitle() {
         ref={confettiRef}
         className="absolute top-0 left-0 z-0 size-full"
         onMouseEnter={() => {
-          confettiRef.current?.fire({})
+          confettiRef.current?.fire({});
         }}
         options={{
           get colors() {
-            return ["#F97316", "#FB923C", "#FCD34D", "#34D399", "#60A5FA", "#A78BFA"]
+            return [
+              "#F97316",
+              "#FB923C",
+              "#FCD34D",
+              "#34D399",
+              "#60A5FA",
+              "#A78BFA",
+            ];
           },
           get shapes() {
             return ["square", "circle"] as Shape[];
@@ -31,17 +38,17 @@ function ConfettiTitle() {
             return 90;
           },
           get particleCount() {
-            return 150
+            return 150;
           },
           get scalar() {
-            return 0.8
+            return 0.8;
           },
           get ticks() {
-            return 300
-          }
+            return 300;
+          },
         }}
       />
-      </div>
+    </div>
   );
 }
 export default ConfettiTitle;
