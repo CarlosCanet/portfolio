62e4f00c0aca997421cd66fb0e6add153df75580
Carlos Canet <carlos@carlosca.net>
2026-02-19 15:40:30 +0100

    fix: correct import path for ConfettiTitle and prepare layout for future components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import ConfettiTitle from "@/features/About/components/ConfettiTitle";
-import ContactCard from "@/features/Contact/components/ContactCard";
-import ProjectListCard from "@/features/Projects/components/ProjectListCard";
-import TechnologiesCard from "@/features/Technologies/components/TechnologiesCard";
+import ConfettiTitle from "@/features/about/components/ConfettiTitle";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-start justify-center mt-10 bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full flex-col items-center justify-start px-8 py-16 bg-white dark:bg-black gap-2">
-        <ConfettiTitle />
-        <TechnologiesCard />
-        <ProjectListCard />
-        <ContactCard />
+        <ConfettiTitle className="-mt-32" />
+        <div className="flex flex-col gap-5 w-full items-center -mt-20 z-10">
+          <div className="flex flex-col md:flex-row mx-5 w-4/5 gap-5"></div>
+          <div className="flex flex-col md:flex-row mx-5 w-4/5 gap-5"></div>
+        </div>
       </main>
     </div>
   );
