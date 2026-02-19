a989838ea952cf04f55a775340341df905f8825c
Carlos Canet <carlos@carlosca.net>
2026-02-19 21:00:37 +0100

    fix: path /feature/about resisting to change and add cn functionality

--- src/features/about/components/ConfettiTitle.tsx
+++ src/features/about/components/ConfettiTitle.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { useRef } from "react";
-import { Confetti, ConfettiRef } from "../../../components/ui/confetti";
+import { Confetti, ConfettiRef } from "@/components/ui/confetti";
 import { Shape } from "canvas-confetti";
+import { cn } from "@/lib/utils";
 
-function ConfettiTitle() {
+interface ConfettiTitleProps {
+  className?: string;
+}
+
+function ConfettiTitle({ className }: ConfettiTitleProps) {
   const confettiRef = useRef<ConfettiRef>(null);
   return (
-    <div className="relative flex h-125 w-full flex-col items-center justify-center overflow-hidden">
+    <div className={cn("relative flex h-125 w-full flex-col items-center justify-center overflow-hidden", className)}>
       <span className="pointer-events-none bg-linear-to-b from-black to-orange-600/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-orange-500 dark:to-orange-500/30">
         Carlos Canet
       </span>
@@ -22,14 +27,7 @@ function ConfettiTitle() {
         }}
         options={{
           get colors() {
-            return [
-              "#F97316",
-              "#FB923C",
-              "#FCD34D",
-              "#34D399",
-              "#60A5FA",
-              "#A78BFA",
-            ];
+            return ["#F97316", "#FB923C", "#FCD34D", "#34D399", "#60A5FA", "#A78BFA"];
           },
           get shapes() {
             return ["square", "circle"] as Shape[];
